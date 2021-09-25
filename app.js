d2d08b6abe7313025d46cbb4be24a67e6d0ae06f
Nabia-sheikh <nabiasheikh2222@gmail.com>
2021-09-25 22:39:56 +0500

    Work done!

---
+++ app.js
@@ -0,0 +1,53 @@
+// Q # 01 write a program which shows the couter after each second.
+// let seconds = 0;
+// function counter() {
+//   seconds += 1;
+//     console.log(seconds);
+// }
+// setInterval(counter, 1000);
+
+// Q # 02 : write a function which takes your name and displays the greeting with your name
+
+// function greetings(name) {
+//     name = prompt("Enter Your Name");
+//     document.write(`<h1> Welcome  ${name} </h1>  `)
+// }
+// greetings();
+
+
+// Q # 03 write a function(arrow function) which takes two values and return its sum as a result
+
+// const sum = (a, b) => {
+//     return a + b
+// }
+
+// console.log(sum(10 , 20));
+
+// Q # 04 write a function(arrow function) which takes a number and multiply it with 0.5 and return the new value. print the new value outside the function
+// const multiply = (numb) => {
+//     return numb * 0.5;
+// }
+// var multiplication = multiply(100)
+// console.log(multiplication);
+
+//Q # 05 print simple array of [1,2,3,4,5] with the help of array map funtion
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.map(
+//     val => console.log(val)
+// )
+
+// Q # 06 let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]  iterate the given array through map function and print the name and id
+
+// let newArr = [
+//   { id: 1, name: "abc" },
+//   { id: 1, name: "efg" },
+//   { id: 2, name: "hij" },
+//   { id: 3, name: "xyz" },
+// ];
+// document.write(`<h4> ID : Name </h4>`)
+// newArr.map(
+//     val => {
+//         document.write( `<h4> ${val.id}  :  ${val.name} </h4>  `  );
+//     }
+// )
